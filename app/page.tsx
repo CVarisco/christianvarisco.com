8e053e36ee303af134fc82bc6257ee7002b6d5f3
CVarisco <vari.chri1993@gmail.com>
2024-04-22 12:09:10 +0200

    feat: wip

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Accordion from "@/components/Accordion";
 
 const sectionClassName =
-  "flex flex-col justify-center items-center container mx-auto space-y-12 md:space-y-20 md:min-h-[80vh] py-20 md:py-0 px-6 md:px-10";
+  "flex flex-col justify-center items-center container mx-auto space-y-12 sm:space-y-20 sm:min-h-[80vh] py-20 sm:py-0 px-6 sm:px-10";
 
 export default function Home() {
   const ref1 = useRef<HTMLDivElement>(null);
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <div className="mx-auto">
       <section id="hero" className="flex flex-col justify-between">
-        <div className="text-left flex flex-col justify-center h-full px-6 md:px-10">
+        <div className="text-left flex flex-col justify-center h-full px-6 sm:px-10">
           <h1 className="text-4xl sm:text-7xl sm:leading-hero font-serif font-semibold">
             Avoid expensive mistakes.
             <br />
@@ -31,15 +31,16 @@ export default function Home() {
             </span>{" "}
             together.
           </h1>
-          <p className="mt-6 text-xl md:text-2xl text-white">
+          <p className="mt-6 text-xl sm:text-2xl text-white">
             Hi, I&apos;m Christian Varisco, your go-to Fractional CTO. Together,
             we&apos;ll turn your
             <br /> groundbreaking ideas into market-ready successes.
           </p>
           <div className="mt-10 flex">
             <a
-              className="bg-lime py-4 px-6 md:px-10 md:py-4 text-black text-xl rounded-full font-serif font-semibold cursor-pointer"
+              className="bg-lime py-4 px-6 sm:px-10 sm:py-4 text-black text-xl rounded-full font-serif font-semibold cursor-pointer"
               href="https://cal.com/christian-varisco/discovery-call"
+              target="_blank"
             >
               Book a FREE call {"->"}
             </a>
@@ -50,7 +51,7 @@ export default function Home() {
         </div>
       </section>
       <section className={sectionClassName}>
-        <h2 className="text-3xl md:text-4xl self-start">
+        <h2 className="text-3xl sm:text-4xl self-start">
           Is your{" "}
           <span className=" font-semibold">
             startup&apos;s product development on the right track?
@@ -58,18 +59,18 @@ export default function Home() {
         </h2>
         <div
           ref={ref1}
-          className={`grid grid-cols-1 md:grid-cols-3 gap-8 transition-opacity ease-in duration-700 ${
+          className={`grid grid-cols-1 sm:grid-cols-3 gap-8 transition-opacity ease-in duration-700 ${
             isVisible1 ? "opacity-100" : "opacity-0"
           }`}
         >
           <div>
             <div className="w-[90%] border-2 border-[#1a1a1a] rounded-xl -mb-4">
-              <h3 className="text-xl md:text-2xl text-lime p-8 md:p-10 font-semibold">
+              <h3 className="text-xl sm:text-2xl text-lime p-8 sm:p-10 font-semibold">
                 Are you navigating without a map?
               </h3>
             </div>
-            <div className="w-full bg-[#111111] p-8 rounded-xl md:min-h-[170px]">
-              <p className="text-lg md:text-xl">
+            <div className="w-full bg-[#111111] p-8 rounded-xl sm:min-h-[170px]">
+              <p className="text-lg sm:text-xl">
                 How often do you find yourself chasing innovation without a
                 clear direction, leading to wasted resources and missed
                 opportunities?
@@ -78,12 +79,12 @@ export default function Home() {
           </div>
           <div>
             <div className="w-[90%] border-2 border-[#1a1a1a] rounded-xl -mb-4">
-              <h3 className="text-xl md:text-2xl text-lime p-8 md:p-10 font-semibold">
+              <h3 className="text-xl sm:text-2xl text-lime p-8 sm:p-10 font-semibold">
                 Are your processes holding you back?
               </h3>
             </div>
-            <div className="w-full bg-[#111111] p-8 rounded-xl md:min-h-[170px]">
-              <p className="text-lg md:text-xl">
+            <div className="w-full bg-[#111111] p-8 rounded-xl sm:min-h-[170px]">
+              <p className="text-lg sm:text-xl">
                 Do you lack a cohesive plan, causing your development to lag and
                 pushing your market entry further away?
               </p>
@@ -91,12 +92,12 @@ export default function Home() {
           </div>
           <div>
             <div className="w-[90%] border-2 border-[#1a1a1a] rounded-xl -mb-4">
-              <h3 className="text-xl md:text-2xl text-lime p-8 md:p-10 font-semibold">
+              <h3 className="text-xl sm:text-2xl text-lime p-8 sm:p-10 font-semibold">
                 Do you have the right team in place?
               </h3>
             </div>
-            <div className="w-full bg-[#111111] p-8 rounded-xl md:min-h-[170px]">
-              <p className="text-lg md:text-xl">
+            <div className="w-full bg-[#111111] p-8 rounded-xl sm:min-h-[170px]">
+              <p className="text-lg sm:text-xl">
                 Are you struggling to find and integrate the tech talent you
                 need, slowing your progress and affecting your product&apos;s
                 potential?
@@ -106,7 +107,7 @@ export default function Home() {
         </div>
       </section>
       <section className={sectionClassName}>
-        <h2 className="text-3xl md:text-4xl font-regular self-start mb-6 md:mb-8">
+        <h2 className="text-3xl sm:text-4xl font-regular self-start mb-6 sm:mb-8">
           <span className="font-semibold">
             Turn obstacles into opportunities
           </span>{" "}
@@ -114,7 +115,7 @@ export default function Home() {
         </h2>
         <div
           ref={ref2}
-          className={`grid grid-cols-1 md:grid-cols-3 gap-8 space-y-10 sm:space-y-0 transition-opacity ease-in duration-700 ${
+          className={`grid grid-cols-1 sm:grid-cols-3 gap-8 space-y-10 sm:space-y-0 transition-opacity ease-in duration-700 ${
             isVisible2 ? "opacity-100" : "opacity-0"
           }`}
         >
@@ -122,10 +123,10 @@ export default function Home() {
             <div className="w-full min-h-[250px] relative">
               <Image src="/images/Isometric.svg" fill alt="service-1" />
             </div>
-            <h3 className="text-xl md:text-2xl font-semibold pt-8">
+            <h3 className="text-xl sm:text-2xl font-semibold pt-8">
               Launch First Product
             </h3>
-            <p className="text-lg md:text-xl">
+            <p className="text-lg sm:text-xl">
               How often do you find yourself chasing innovation without a clear
               direction, leading to wasted resources and missed opportunities?
             </p>
@@ -134,10 +135,10 @@ export default function Home() {
             <div className="w-full min-h-[250px] relative">
               <Image src="/images/Isometric1.svg" fill alt="service-2" />
             </div>
-            <h3 className="text-xl md:text-2xl font-semibold pt-8">
+            <h3 className="text-xl sm:text-2xl font-semibold pt-8">
               Navigate Technical Strategy
             </h3>
-            <p className="text-lg md:text-xl">
+            <p className="text-lg sm:text-xl">
               Navigate the complexities of technology, architecture, and
               development processes with expert guidance.
             </p>
@@ -146,83 +147,128 @@ export default function Home() {
             <div className="w-full min-h-[250px] relative">
               <Image src="/images/Isometric2.svg" fill alt="service-3" />
             </div>
-            <h3 className="text-xl md:text-2xl font-semibold pt-8">
+            <h3 className="text-xl sm:text-2xl font-semibold pt-8">
               Build Dream Team
             </h3>
-            <p className="text-lg md:text-xl">
+            <p className="text-lg sm:text-xl">
               Identify and integrate the right technical skills, streamlining
               your path to innovation and growth.
             </p>
           </div>
         </div>
       </section>
+      <section
+        className={`flex justify-center items-center container mx-auto space-y-12 sm:space-y-0 sm:min-h-[80vh] py-20 sm:py-0 px-6 sm:px-10`}
+      >
+        <div className="flex">
+          <div>
+            <h2 className="text-3xl sm:text-4xl self-start">
+              3 steps to{" "}
+              <span className=" font-semibold">transform your startup</span>
+            </h2>
+          </div>
+          <div
+            className={`flex flex-col gap-8 transition-opacity ease-in duration-700`}
+          >
+            <div className="flex">
+              <div className="border-2 border-[#1a1a1a] rounded-[32px] rounded-r-lg px-6 pt-6 border-r-0 h-24 min-w-[130px] -mr-8">
+                <div className="bg-lime rounded-full w-12 h-12 flex justify-center items-center">
+                  <span className="text-2xl sm:text-3xl text-black font-serif font-semibold">
+                    1
+                  </span>
+                </div>
+              </div>
+              <div className="w-full bg-[#111111] p-8 rounded-[32px] space-y-6 sm:min-h-[170px]">
+                <h3 className="text-xl sm:text-2xl font-semibold">
+                  Book a call
+                </h3>
+                <p className="text-lg sm:text-xl">
+                  Schedule a free discovery session with me through my link,
+                  fitting your timetable.
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="border-2 border-[#1a1a1a] rounded-[32px] rounded-r-lg px-6 pt-6 border-r-0 h-24 min-w-[130px] -mr-8">
+                <div className="bg-lime rounded-full w-12 h-12 flex justify-center items-center">
+                  <span className="text-2xl sm:text-3xl text-black font-serif font-semibold">
+                    2
+                  </span>
+                </div>
+              </div>
+              <div className="w-full bg-[#111111] p-8 rounded-[32px] space-y-6 sm:min-h-[170px]">
+                <h3 className="text-xl sm:text-2xl font-semibold">
+                  Discuss Goals
+                </h3>
+                <p className="text-lg sm:text-xl">
+                  Let&apos;s Talk about your startup&apos;s and technical
+                  challenges with me during the call.
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="border-2 border-[#1a1a1a] rounded-[32px] rounded-r-lg px-6 pt-6 border-r-0 h-24 min-w-[130px] -mr-8">
+                <div className="bg-lime rounded-full w-12 h-12 flex justify-center items-center">
+                  <span className="text-2xl sm:text-3xl text-black font-serif font-semibold">
+                    3
+                  </span>
+                </div>
+              </div>
+              <div className="w-full bg-[#111111] p-8 rounded-[32px] space-y-6 sm:min-h-[170px]">
+                <h3 className="text-xl sm:text-2xl font-semibold">
+                  Execute & Launch
+                </h3>
+                <p className="text-lg sm:text-xl">
+                  Receive a tailor made offer to move your startup forward
+                  effectively.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className={sectionClassName}>
-        <h2 className="text-3xl md:text-4xl self-start">
-          3 steps to{" "}
-          <span className=" font-semibold">transform your startup</span>
-        </h2>
-        <div
-          ref={ref3}
-          className={`grid grid-cols-1 md:grid-cols-3 gap-8 transition-opacity ease-in duration-700 ${
-            isVisible3 ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          <div>
-            <div className="w-[90%] border-2 border-[#1a1a1a] rounded-xl -mb-4 px-4 py-8">
-              <div className="bg-lime rounded-full w-12 h-12 flex justify-center items-center">
-                <span className="text-2xl md:text-3xl text-black font-serif font-semibold">
-                  1
-                </span>
-              </div>
-            </div>
-            <div className="w-full bg-[#111111] p-8 rounded-xl space-y-6 md:min-h-[170px]">
-              <h3 className="text-xl md:text-2xl font-semibold">Book a call</h3>
-              <p className="text-lg md:text-xl">
-                Schedule a free discovery session with me through my link,
-                fitting your timetable.
-              </p>
+        <div className={`flex gap-8 transition-opacity ease-in duration-700 `}>
+          <div className="w-full min-h-[250px] md:min-h-[350px] rounded-xl relative min-w-32">
+            <div>
+              <Image
+                src="/images/me.jpg"
+                fill
+                alt="me"
+                className="rounded-xl"
+              />
             </div>
           </div>
           <div>
-            <div className="w-[90%] border-2 border-[#1a1a1a] rounded-xl -mb-4 px-4 py-8">
-              <div className="bg-lime rounded-full w-12 h-12 flex justify-center items-center">
-                <span className="text-2xl md:text-3xl text-black font-serif font-semibold">
-                  2
-                </span>
-              </div>
+            <div className="w-[90%] border-2 border-[#1a1a1a] rounded-xl -mb-4 px-6 py-8">
+              <h2 className="text-3xl md:text-4xl self-start text-lime font-semibold">
+                About me
+              </h2>
             </div>
             <div className="w-full bg-[#111111] p-8 rounded-xl space-y-6 md:min-h-[170px]">
-              <h3 className="text-xl md:text-2xl font-semibold">
-                Discuss Goals
-              </h3>
               <p className="text-lg md:text-xl">
-                Let&apos;s Talk about your startup&apos;s and technical
-                challenges with me during the call.
-              </p>
-            </div>
-          </div>
-          <div>
-            <div className="w-[90%] border-2 border-[#1a1a1a] rounded-xl -mb-4 px-4 py-8">
-              <div className="bg-lime rounded-full w-12 h-12 flex justify-center items-center">
-                <span className="text-2xl md:text-3xl text-black font-serif font-semibold">
-                  3
-                </span>
-              </div>
-            </div>
-            <div className="w-full bg-[#111111] p-8 rounded-xl space-y-6 md:min-h-[170px]">
-              <h3 className="text-xl md:text-2xl font-semibold">
-                Execute & Launch
-              </h3>
-              <p className="text-lg md:text-xl">
-                Receive a tailor made offer to move your startup forward
-                effectively.
+                Hi, I&apos;m Christian and I help startups through their tech
+                challenges. I am a passionate tech startup enthusiast with a
+                decade of experience in the field. Over the years, I have had
+                the privilege of working in various startups, both as a
+                developer and as a manager/founder.I also had a significant role
+                in a venture builder, where I was tasked with crafting the
+                technical aspects of new startups. Throughout this ten-year
+                journey, I&apos;ve honed my skills in building tech startups.The
+                value I bring month by month is shielding you from expensive
+                mistakes that can derail your startup. Together, we&apos;ll
+                speed up your time to market, bringing your vision into the
+                hands of your users. I&apos;ll onboard and grow your talent on
+                the right path, nurturing their potential for long-term
+                success.My expertise guarantees a rock-solid technical strategy,
+                transforming your startup into a robust and competitive reality.
               </p>
             </div>
           </div>
         </div>
       </section>
       <section className={sectionClassName}>
-        <h2 className="text-3xl md:text-4xl font-semibold">
+        <h2 className="text-3xl sm:text-4xl font-semibold">
           Frequently asked questions
         </h2>
         <div>
